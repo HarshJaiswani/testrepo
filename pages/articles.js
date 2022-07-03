102a99a0d93d481a72fa7a57e41fa8356150c226
Harsh Jaiswani <harshjaiswani2003@gmail.com>
2022-07-03 10:32:45 +0530

    version 1.0.0

--- pages/articles.js
+++ pages/articles.js
@@ -15,23 +15,25 @@ const Articles = (props) => {
     <div className={styles.articles}>
       <Heading name="The Essence of Learning" tagLine="The best way to experience other's life , Is to READ them" img="articles" note="I am a 18 years old boy, I am not someone the age of say 40+ or something so that , I could be able to share my experince but the day we are born we start experincing and learning things. These artiles will act as a medium for me to share my journey , learning and specially my mistakes to try and help you perform a bit better in you life. Hope you will enjoy !" />
       <div className={styles.articleWrapper}>
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
-        <ArticleBlockTemplate article={article} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
+        <ArticleBlockTemplate article={props.blog} />
       </div>
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
-  return {
-    props: {abcd:"hllo"},
+  const data = await fetch(`http://localhost:3000/api/getBlog`);
+  const blog = await data.json();
+  return{
+    props : {blog}
   }
 }
 
