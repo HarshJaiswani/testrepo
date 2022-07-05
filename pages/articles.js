d100f34967e1af151bf0c70781dc0e4405558db0
Harsh Jaiswani <harshjaiswani2003@gmail.com>
2022-07-05 07:38:28 +0530

    version 1.0.0

--- pages/articles.js
+++ pages/articles.js
@@ -23,7 +23,7 @@ const Articles = (props) => {
 }
 
 export async function getServerSideProps(context) {
-  const data = await fetch(`public/api/getBlog?slug=article`);
+  const data = await fetch(`${process.env.host}/api/getBlog?slug=article`);
   const blog = await data.json();
   return{
     props : {blog}
