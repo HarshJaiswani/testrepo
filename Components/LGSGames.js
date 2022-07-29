c88efda49e65dea72f798415d6e77d30ac26c7b3
Harsh <harshjaiswani2003@gmail.com>
2022-07-29 16:28:38 +0530

    Game Sections Responsive

--- Components/LGSGames.js
+++ Components/LGSGames.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const LGSGames = () => {
   return (
     <div className={styles.lgsgames}>
-      <h1>LGS Games <BsEyeglasses className='text-9xl inline text-yellow-500' /></h1>
+      <h1>LGS Games <BsEyeglasses className='text-6xl sm:text-9xl inline text-yellow-500' /></h1>
       <Link href="/games"><a className={styles.btn}>Explore</a></Link>
     </div>
   )
