80d55e258c79d96abaaba75a61b1fa4179370d34
Harsh <harshjaiswani2003@gmail.com>
2022-08-18 10:28:59 +0530

    Minutes Read Added and Chapter 2 Added

--- Components/ArticleBlockTemplate.js
+++ Components/ArticleBlockTemplate.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import styles from "../styles/ArticleBlockTemplate.module.css";
 import Link from "next/link";
 import { BiChevronRight } from "react-icons/bi";
 
 const ArticleBlockTemplate = (props) => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    props.article.parts.forEach(element => {
+      setCount(count + element.length);
+    });
+  }, [])
+  
   return (
     <div className={styles.articleBlock} key={props.article.slug}>
       <div className={styles.articleDate}>
@@ -11,6 +18,7 @@ const ArticleBlockTemplate = (props) => {
       </div>
       <div className={styles.articleTitle}>{props.article.title}</div>
       <div className={styles.articleDescp}>{props.article.descp}...</div>
+      <div className="text-gray-400 font-semibold text-sm tracking-wider">Minutes Read - <b className="mr-2 text-white">{(count/120).toFixed(2)}</b></div>
       <Link href={props.article.link}>
         <a className={styles.link}>
           Read More{" "}
