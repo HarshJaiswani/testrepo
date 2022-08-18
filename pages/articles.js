340bc1a96eb74b9209bfe4d519456e5af72136e6
Harsh <harshjaiswani2003@gmail.com>
2022-08-18 19:51:19 +0530

    Spelling Corrected

--- pages/articles.js
+++ pages/articles.js
@@ -24,7 +24,7 @@ const Articles = () => {
   }, [error, data]);
   return (
     <div className={styles.articles}>
-      <Heading name="The Essence of Learning" tagLine="The best way to experience other's life , Is to READ them" img="articles" note="I am a 18 years old boy, I am not someone the age of say 40+ or something so that , I could be able to share my experince but the day we are born we start experincing and learning things. These artiles will act as a medium for me to share my journey , learning and specially my mistakes to try and help you perform a bit better in you life. Hope you will enjoy !" />
+      <Heading name="The Essence of Learning" tagLine="The best way to experience other's life , Is to READ them" img="articles" note="I am a 18 years old boy, I am not someone the age of say 40+ or something so that , I could be able to share my experience but the day we are born we start experiencing and learning things. These artiles will act as a medium for me to share my journey , learning and specially my mistakes to try and help you perform a bit better in you life. Hope you will enjoy !" />
       <div className={styles.articleWrapper}>
       {isOk == "false" ? (
           <div className="w-full text-2xl text-center my-5 text-yellow-500">
